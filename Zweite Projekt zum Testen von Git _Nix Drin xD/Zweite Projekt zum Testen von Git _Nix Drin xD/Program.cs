e61e8214b3e943f174927e031549fef672757b83
zacbang <christopherzacsek@gmx.at>
2016-01-09 03:46:56 +0100

    Test

---
+++ Zweite Projekt zum Testen von Git _Nix Drin xD/Zweite Projekt zum Testen von Git _Nix Drin xD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zweite_Projekt_zum_Testen_von_Git__Nix_Drin_xD
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
